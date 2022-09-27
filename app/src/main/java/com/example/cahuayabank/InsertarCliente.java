9171d832ab17e58868553b31fcefdb19165e3cba
WilCahuaya <48217068@continental.edu.pe>
2022-09-26 19:17:08 -0500

    Start Cahuaya Bank

---
+++ app/src/main/java/com/example/cahuayabank/InsertarCliente.java
@@ -0,0 +1,14 @@
+package com.example.cahuayabank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertarCliente extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_cliente);
+    }
+}
